d9d4b908e04d6adf5afa2abace83799db32089b7
HoLLy <dario.db@hotmail.com>
2023-05-14 23:12:26 +0200

    Replace demo app with simple text transformation

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,38 @@
+use web_sys::HtmlTextAreaElement;
 use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let textarea_ref = use_node_ref();
+    let text = use_state(|| String::from("Initial text"));
+
+    // onkeypress would be delayed by 1 character
+    let onkeyup = {
+        let text = text.clone();
+        let textarea_ref = textarea_ref.clone();
+        move |_| {
+            let new_value = textarea_ref.cast::<HtmlTextAreaElement>().unwrap().value();
+            let new_value = transform_text(&new_value);
+            text.set(new_value);
+        }
+    };
+
     html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
+        <div>
+            <textarea ref={textarea_ref} {onkeyup} />
+            <pre>{ format!("{}", *text) }</pre>
+        </div>
     }
+}
+
+fn transform_text(text: &str) -> String {
+    text.chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_ascii_lowercase()
+            } else {
+                c.to_ascii_uppercase()
+            }
+        })
+        .collect()
 }
